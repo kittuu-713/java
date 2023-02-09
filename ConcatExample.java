95e71d4c05e4dbf4d10ba6a21535fc281f337423
Kommula Sri Datta Sai Krishna <124143634+kittuu-713@users.noreply.github.com>
2023-02-09 14:55:49 +0530

    Add files via upload

---
+++ ConcatExample.java
@@ -0,0 +1,8 @@
+package StringFunctions;
+
+public class ConcatExample{
+    public static void concat(){
+        String s1="java string";
+        s1=s1.concat(" is immutable so assign it explicitly");
+        System.out.println(s1);
+    }}
