34587665df2cf710d3d2eda86378e3196db5d19c
Kommula Sri Datta Sai Krishna <124143634+kittuu-713@users.noreply.github.com>
2023-02-10 20:54:34 +0530

    Create string12.java

---
+++ src/src/string12.java
@@ -0,0 +1,8 @@
+public class string12 {public static void main(String args[]){
+    String s1="";
+    String s2="javatpoint";
+
+    System.out.println(s1.isEmpty());
+    System.out.println(s2.isEmpty());
+}
+}
